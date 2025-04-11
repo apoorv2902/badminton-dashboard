9097ca8b419736745062c7df9cb943d87b0004fa
unknown <apoorv2902@gmail.com>
2025-04-11 16:56:33 +0530

    Fixed json reading issue in connector app

--- sheets_connector.py
+++ sheets_connector.py
@@ -5,7 +5,7 @@ import json
 
 def connect_to_sheets(secret_section_name: str, sheet_name: str):
     scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
-    credentials_dict = st.secrets[secret_section_name]
+    credentials_dict = json.loads(st.secrets[secret_section_name])    
     creds = ServiceAccountCredentials.from_json_keyfile_dict(credentials_dict, scope)
     client = gspread.authorize(creds)
     sheet = client.open(sheet_name).sheet1
