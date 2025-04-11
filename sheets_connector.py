e906717c902eff1b52820a06db2051e58e7d7c21
unknown <apoorv2902@gmail.com>
2025-04-11 16:46:28 +0530

    Fixed sync issue and added updates

--- sheets_connector.py
+++ sheets_connector.py
@@ -1,15 +1,18 @@
-import gspread
+iimport os
+import json
+
 from oauth2client.service_account import ServiceAccountCredentials
+import gspread
 import pandas as pd
 
-def connect_to_sheets(json_keyfile_path, sheet_name):
-    scope = ['https://spreadsheets.google.com/feeds',
-             'https://www.googleapis.com/auth/drive']
+def connect_to_sheets(sheet_name):
+    scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
     
-    creds = ServiceAccountCredentials.from_json_keyfile_name(json_keyfile_path, scope)
+    service_account_info = json.loads(os.environ["gcp_service_account"])
+    creds = ServiceAccountCredentials.from_json_keyfile_dict(service_account_info, scope)
     client = gspread.authorize(creds)
 
-    sheet = client.open(sheet_name).sheet1  # Or use get_worksheet(0)
+    sheet = client.open(sheet_name).sheet1
     return sheet
 
 def get_scores_df(sheet):
